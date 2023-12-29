ccdea1dc3c76acc96d89306c0a3fb91cac0c2366
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2023-12-28 22:41:27 -0500

    some reminders for later

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 #![feature(generic_const_exprs)]
 #![allow(incomplete_features)]
+// #![feature(associated_type_defaults)]
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+// pub mod functor;
 pub mod layout;
 
 #[cfg(test)]
